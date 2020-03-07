80ecc7354fd58a395154d4a0be8778bf6b7f9357
Cristian Gonzalez <crdgonzalezca@unal.edu.co>
2020-03-07 01:22:51 -0500

    Fix compilation error custom input returned data and stop running a second container in multilang

--- grading/uncode/grading/projects.py
+++ grading/uncode/grading/projects.py
@@ -18,7 +18,7 @@ def _run_in_sandbox(command, **subprocess_options):
     subprocess_options -- Additional options sent to subprocess.run.
     """
 
-    completed_process = subprocess.run(["run_student"] + command, stdout=subprocess.PIPE,
+    completed_process = subprocess.run(command, stdout=subprocess.PIPE,
         stderr=subprocess.PIPE, **subprocess_options)
 
     stdout = completed_process.stdout.decode()
