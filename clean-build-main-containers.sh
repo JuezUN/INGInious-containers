1a1576dc2b7209da811503aa862ba310a5c1157a
Gabriel <gaperezsa@unal.edu.co>
2022-07-21 15:19:13 -0500

    clean building uncode base, multilang,data_science,notebook and hdl

---
+++ clean-build-main-containers.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -e
+
+grading=("datascience" "notebook" "multilang")
+
+for container in "${grading[@]}"; do
+        echo "-------------------------------------------------------"
+        echo "- Destroing grading image: " "$container"
+        echo "-------------------------------------------------------"
+        docker rmi -f ingi/inginious-c-"$container"
+done
+
+echo "-------------------------------------------------------"
+echo "- Destroing grading image: hdl"
+echo "-------------------------------------------------------"
+docker rmi -f ingi/hdl-uncode
+echo "-------------------------------------------------------"
+echo "- Destroing grading image: uncode-c-base"
+echo "-------------------------------------------------------"
+docker rmi -f unjudge/uncode-c-base
+
+
+echo "-------------------------------------------------------"
+echo "- Building grading image: uncode-c-base"
+echo "-------------------------------------------------------"
+docker build -t "unjudge/uncode-c-base" "grading/uncode"
+
+echo "-------------------------------------------------------"
+echo "- Building grading image: hdl"
+echo "-------------------------------------------------------"
+docker build -t "ingi/hdl-uncode" "grading/hdl"
+
+grading=( "notebook" "multilang" "data_science")
+
+for container in ${grading[@]}; do
+        echo "-------------------------------------------------------"
+        echo "- Building grading image:" "$container"
+        echo "-------------------------------------------------------"
+        docker build -t "ingi/inginious-c-"$container"" "grading/"$container""
+done
+
+
